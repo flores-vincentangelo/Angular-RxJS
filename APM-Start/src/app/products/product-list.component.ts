84189b0fba0e8a5a5892ef72343f52f2c4d29a0a
flores-vincentangelo <vincent.flores@pointwest.com.ph>
2022-02-23 17:59:14 +0800

    updates

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import { catchError, EMPTY, Observable, of } from 'rxjs';
 
-import { Subscription } from 'rxjs';
+// import { Subscription } from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
 
 import { Product } from './product';
@@ -8,29 +9,40 @@ import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent implements OnInit/*,  OnDestroy */ {
   pageTitle = 'Product List';
   errorMessage = '';
   categories: ProductCategory[] = [];
 
-  products: Product[] = [];
-  sub!: Subscription;
+  // products: Product[] = [];
+  // sub!: Subscription;
+
+  products$: Observable<Product[]> | undefined;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.sub = this.productService.getProducts()
-      .subscribe({
-        next: products => this.products = products,
-        error: err => this.errorMessage = err
-      });
+    // this.sub = this.productService.getProducts()
+    //   .subscribe({
+      //     next: products => this.products = products,
+      //     error: err => this.errorMessage = err
+      //   });
+      this.products$ = this.productService.getProducts()
+      .pipe(
+        catchError( err => {
+          this.errorMessage = err;
+          // return of([]);
+          return EMPTY;
+        })
+      );
   }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
+  // ngOnDestroy(): void {
+  //   this.sub.unsubscribe();
+  // }
 
   onAdd(): void {
     console.log('Not yet implemented');

cd9b6a6357fd89949bf8fdff3ad5613ce101a318
flores-vincentangelo <vincent.flores@pointwest.com.ph>
2022-03-04 19:49:48 +0800

    updates

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
-import { BehaviorSubject, catchError, combineLatest, EMPTY, filter, map, Observable, of, Subject } from 'rxjs';
+import { catchError, combineLatest, EMPTY, filter, map, Observable, of, startWith, Subject } from 'rxjs';
 
 // import { Subscription } from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
@@ -21,12 +21,15 @@ export class ProductListComponent /* implements OnInit,  OnDestroy */ {
   // products: Product[] = [];
   // sub!: Subscription;
 
-  private categorySelectedSubject = new BehaviorSubject<number>(0);
+  private categorySelectedSubject = new Subject<number>();
   categorySelectedAction$ = this.categorySelectedSubject.asObservable();
 
   products$ = combineLatest([
     this.productService.productsWithCategory$,
     this.categorySelectedAction$
+    .pipe(
+      startWith(0)
+    )
   ])
     .pipe(
       map(([products, selectedCategoryId]) => products.filter(
